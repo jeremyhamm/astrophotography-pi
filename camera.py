879c104f8f48e221b89a21e9fe05da6a45a5c525
Jeremy Hamm <jhamm@platformscience.com>
2021-01-08 15:38:25 -0800

    Add test for saving image to external drive

--- camera.py
+++ camera.py
@@ -3,9 +3,13 @@ from PIL import Image
 from time import sleep
 
 camera = picamera.PiCamera()
-camera.resolution = (3280, 2464)
+camera.resolution = '3280x2464'
 camera.framerate = 15
 camera.start_preview()
+
+# Camera warm-up time
+sleep(2)
+camera.capture('/CONFIG/foo.jpg')
 
 # Wait indefinitely until the user terminates the script
 while True:
