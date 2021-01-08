0512e29befa26600098ecbfc86dd7c10dd36cb24
Jeremy Hamm <jhamm@platformscience.com>
2021-01-08 14:26:18 -0800

    Update resolution and framerate

--- camera.py
+++ camera.py
@@ -3,8 +3,8 @@ from PIL import Image
 from time import sleep
 
 camera = picamera.PiCamera()
-camera.resolution = (1280, 720)
-camera.framerate = 24
+camera.resolution = (3280, 2464)
+camera.framerate = 15
 camera.start_preview()
 
 # Wait indefinitely until the user terminates the script
