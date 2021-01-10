4a0767b831361ca5a1803f87932ec473d9462f28
Jeremy Hamm <jh@jeremyhamm.dev>
2021-01-10 13:06:00 -0800

    Add env variables for initiating camera

--- camera.py
+++ camera.py
@@ -1,16 +1,23 @@
+import os
 import picamera
 from PIL import Image
 from time import sleep
 
+# Env variables
+os.environ['resolution'] = '3280x2464'
+os.environ['framerate'] = 15
+os.environ['output_location'] = '/media/pi/CONFIG/'
+
+# Camera Init
 camera = picamera.PiCamera()
-camera.resolution = '3280x2464'
-camera.framerate = 15
+camera.resolution = os.environ.get('resolution')
+camera.framerate = os.environ.get('framerate')
 camera.start_preview()
 
 # Camera warm-up time
 sleep(2)
-camera.capture('/media/pi/CONFIG/bar.jpg')
+camera.capture(os.environ.get('output_location') + 'bar.jpg')
 
 # Wait indefinitely until the user terminates the script
 while True:
-    sleep(1)
+  sleep(1)
