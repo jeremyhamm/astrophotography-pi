de0ea5280899bc709472bc11f62354420155c604
Jeremy Hamm <jhamm@platformscience.com>
2021-01-12 13:39:55 -0800

    Limit number of images to env var

--- camera.py
+++ camera.py
@@ -6,6 +6,7 @@ from time import sleep
 # Env variables
 os.environ['resolution'] = '3280x2464'
 os.environ['output_location'] = '/media/pi/CONFIG'
+os.environ['session_limit'] = 5
 
 # Camera Init
 camera = picamera.PiCamera()
@@ -15,8 +16,13 @@ camera.start_preview()
 # Camera warm-up time
 print('Camera starting...')
 sleep(2)
+counter = 0
 for filename in camera.capture_continuous(str(os.environ.get('output_location')) + '/image{counter}.jpg'):
+  counter++
+  if (counter != os.environ['session_limit'])
     print(str(os.environ.get('output_location')) + 'image{counter}.jpg')
+  else
+    break
 
 camera.close()
 print('Complete')
