138df7ab7c0271e737a9a3aff6498a899d445bf3
Jeremy Hamm <jhamm@platformscience.com>
2021-01-12 13:52:43 -0800

    Functionize sequence

--- camera.py
+++ camera.py
@@ -13,16 +13,19 @@ camera = picamera.PiCamera()
 camera.resolution = os.environ.get('resolution')
 camera.start_preview()
 
-# Camera warm-up time
+def startSequence():
+  counter = 0
+  for filename in camera.capture_continuous(str(os.environ.get('output_location')) + '/image{counter}.jpg'):
+    counter += 1
+    if (counter != int(os.environ['session_limit'])):
+      print(filename)
+    else:
+      break
+
+# Start camera sequence
 print('Camera starting...')
 sleep(2)
-counter = 0
-for filename in camera.capture_continuous(str(os.environ.get('output_location')) + '/image{counter}.jpg'):
-  counter += 1
-  if (counter != int(os.environ['session_limit'])):
-    print(filename)
-  else:
-    break
+startSequence()
 
 camera.close()
 print('Complete')
