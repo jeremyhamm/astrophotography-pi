d1cc8c65ed6ff0373153bf2c81da9f906b5baca9
Jeremy Hamm <jhamm@platformscience.com>
2021-01-12 13:00:05 -0800

    Update output location

--- camera.py
+++ camera.py
@@ -5,20 +5,18 @@ from time import sleep
 
 # Env variables
 os.environ['resolution'] = '3280x2464'
-os.environ['framerate'] = '15'
 os.environ['output_location'] = '/media/pi/CONFIG'
 
 # Camera Init
 camera = picamera.PiCamera()
 camera.resolution = os.environ.get('resolution')
-camera.framerate = os.environ.get('framerate')
 camera.start_preview()
 
 # Camera warm-up time
 sleep(2)
 for counter in range(5):
     print(counter)
-    camera.capture_continuous('{path}/image{counter}.jpg'.format(path=os.environ.get('output_location'))
+    camera.capture_continuous(str(os.environ.get('output_location')) + '/image' + str(counter) + '.jpg')
 
 # Wait indefinitely until the user terminates the script
 while True:
