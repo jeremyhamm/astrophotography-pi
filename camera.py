68be20ec016c8f85defac45895a6cebd496af45d
Jeremy Hamm <jhamm@platformscience.com>
2021-01-12 13:24:20 -0800

    Update filename for images

--- camera.py
+++ camera.py
@@ -15,8 +15,8 @@ camera.start_preview()
 # Camera warm-up time
 print('Camera starting...')
 sleep(2)
-for filename in camera.capture_continuous(str(os.environ.get('output_location')) + '/image' + str(counter) + '.jpg'):
-    print(str(os.environ.get('output_location')) + '/image' + str(counter) + '.jpg')
+for filename in camera.capture_continuous(str(os.environ.get('output_location')) + 'image{counter}.jpg'):
+    print(str(os.environ.get('output_location')) + 'image{counter}.jpg')
 
 camera.close()
 print('Complete')
